93052dd8662593828df95143e23478c0725a52b0
Minh Nghia <minhnghiacan25032003@gmail.com>
2022-07-08 19:51:02 +0700

    Fixed...

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver } from '@nestjs/apollo';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { MatchesModule } from './matches/matches.module';
 import { RoundsModule } from './rounds/rounds.module';
@@ -10,10 +10,12 @@ import { StandingsModule } from './standings/standings.module';
 @Module({
   imports: [
     MatchesModule,
-    GraphQLModule.forRoot({
+    GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
+      playground: true,
+      introspection: true,
     }),
     RoundsModule,
     ResolveModule,
